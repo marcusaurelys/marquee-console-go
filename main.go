a0d2ceb3b46b029d2e642dd33018f23d41c422a2
Joel Ethan Batac <bjoelethan@gmail.com>
2025-05-15 15:30:40 +0800

    refactored the terminal state into its own struct

--- main.go
+++ main.go
@@ -11,6 +11,75 @@ import (
 	"golang.org/x/sys/windows"
 )
 
+type TerminalState struct {
+	headerState string
+	inputState  []string
+	keyhit      chan string
+	refreshRate int
+	startPad    int
+	text        string
+}
+
+func (ts *TerminalState) updateTerminalHeader() {
+	terminalState := &ts.headerState
+	start := &ts.startPad
+	text := ts.text
+	for {
+		padded := strings.Repeat(" ", *start) + text
+		*terminalState = padded
+		time.Sleep(time.Duration(ts.refreshRate) * time.Millisecond)
+		*start++
+	}
+
+}
+
+func (ts *TerminalState) repaint() {
+	input := ts.inputState
+	terminalState := &ts.headerState
+	keyhit := ts.keyhit
+
+	for {
+
+		fmt.Print(*terminalState + "\n~" + strings.Join(input, "\n~"))
+		select {
+
+		case val := <-keyhit:
+
+			if val == "\r" {
+				if input[len(input)-1] == "clear" {
+					input = make([]string, 0)
+				}
+				input = append(input, "")
+			} else if val == "\b" && len(input[len(input)-1]) >= 1 {
+				in := input[len(input)-1]
+				input[len(input)-1] = in[:len(input[len(input)-1])-1]
+				setCursor()
+			} else {
+				input[len(input)-1] += val
+			}
+			clear()
+
+		case <-time.After(time.Duration(ts.refreshRate) * time.Millisecond):
+			clear()
+		}
+
+	}
+}
+
+func (ts *TerminalState) setInputReader() {
+	input := ts.keyhit
+	reader := bufio.NewReader(os.Stdin)
+	var buf [1]byte
+	for {
+		n, err := reader.Read(buf[:])
+		if err != nil {
+			continue
+		}
+		input <- string(buf[:n])
+	}
+
+}
+
 func disableEcho() {
 	// Get the handle to standard input
 	handle := windows.Handle(windows.Stdin)
@@ -60,74 +129,18 @@ func clear() {
 	cmd.Run()
 }
 
-func repaint(terminalState *string, input []string, keyhit chan string) {
-	for {
-
-		fmt.Print(*terminalState + "\n~" + strings.Join(input, "\n~"))
-		select {
-
-		case val := <-keyhit:
-
-			if val == "\r" {
-				if input[len(input)-1] == "clear" {
-					input = input[:0]
-				}
-				input = append(input, "")
-			} else if val == "\b" && len(input[len(input)-1]) != 0 {
-				in := input[len(input)-1]
-				input[len(input)-1] = in[:len(input[len(input)-1])-1]
-			} else {
-				input[len(input)-1] += val
-			}
-			clear()
-
-		case <-time.After(500 * time.Millisecond):
-			clear()
-		}
-
-	}
-}
-
-func updateTerminalHeader(start *int, terminalState *string, text string) {
-	for {
-		padded := strings.Repeat(" ", *start) + text
-		*terminalState = padded
-		time.Sleep(500 * time.Millisecond)
-		*start++
-	}
-
-}
-
-func inputReader(input chan string) {
-	reader := bufio.NewReader(os.Stdin)
-	var buf [1]byte
-	for {
-		n, err := reader.Read(buf[:])
-		if err != nil {
-			continue
-		}
-		input <- string(buf[:n])
-	}
-
-}
-
 func main() {
 	disableEcho()
-	headerState := "CSOPESY"
-	input := []string{""}
 	clear()
 
-	keyhit := make(chan string)
+	ts := TerminalState{"CSOPESY", []string{""}, make(chan string), 100, 0, "CSOPESY"}
 
-	start := 0
-
-	go updateTerminalHeader(&start, &headerState, "CSOPESY")
-	go inputReader(keyhit)
-	go repaint(&headerState, input, keyhit)
+	go ts.updateTerminalHeader()
+	go ts.setInputReader()
+	go ts.repaint()
 	//fmt.Println("Type something (you have 5 seconds):")
 
 	<-time.After(20 * time.Second)
 	fmt.Println("\ninput!")
-	fmt.Println(strings.Join(input, "\n"))
 
 }
